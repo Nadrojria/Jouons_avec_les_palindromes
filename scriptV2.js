93af77f4a3c13727c606816fd82a5dcf20ddda1d
Nadrojria <loicdesbordes@yahoo.fr>
2025-03-13 10:33:35 +0100

    step 2 : OK

--- scriptV2.js
+++ scriptV2.js
@@ -1,23 +1,5 @@
 let maxDays;
 
-function isValidDate (date) {
-    let result = date.split("/");
-    let dayValid = maxDaysInMonth(result[1], result[2]);
-
-    if (date.length != 10) {
-        console.log("Retourne une date au format dd/mm/yyyy");
-    } else if (result[0] > dayValid) {
-        console.log("jour invalide");
-    } else if (result[1] < 1 || result[1] > 12) {
-        console.log("mois invalide");
-    } else if (result[2] < 1000 || result[2] > 9999) {
-        console.log("année invalide");
-    } else {
-        let finalDate = `${result[0]}${result[1]}${result[2]}`;
-        console.log(finalDate);
-    }
-}
-
 function maxDaysInMonth (month, year) {
     if (month == 4 || month == 6 || month == 9 || month == 11) {
         maxDays = 30;
@@ -31,9 +13,51 @@ function maxDaysInMonth (month, year) {
     return maxDays;
 }
 
-isValidDate("24/011/1992");
-isValidDate("31/06/2024");
-isValidDate("32/10/2024");
-isValidDate("31/13/2024");
-isValidDate("29/02/2025");
-isValidDate("29/02/2024");
+function isValidDate (date) {
+    let result = date.split("/");
+    let dayValid = maxDaysInMonth(result[1], result[2]);
+
+    if (date.length != 10) {
+        console.log("Retourne une date au format dd/mm/yyyy");
+        return false;
+    } else if (result[0] > dayValid) {
+        console.log(`${result[0]}/${result[1]}/${result[2]} a le jour invalide`);
+        return false;
+    } else if (result[1] < 1 || result[1] > 12) {
+        console.log(`${result[0]}/${result[1]}/${result[2]} a le mois invalide`);
+        return false;
+    } else if (result[2] < 1000 || result[2] > 9999) {
+        console.log(`${result[0]}/${result[1]}/${result[2]} a l'année invalide`);
+        return false;
+    } else {
+        return true;
+    }
+}
+
+function isPalindrom(date) {
+    let validDate = isValidDate(date);
+    if (validDate === true) {
+        let finalDate = date.split("/").join("");
+
+        let splitDate = date.split("/");
+        let arrayDate = `${splitDate[0]}${splitDate[1]}${splitDate[2]}`;
+        let reverseDate = arrayDate.split("").reverse().join("");
+
+        if (finalDate === reverseDate) {
+            console.log(`${date} est un palindrome`);
+            return true
+        } else {
+            console.log(`${date} n'est pas un palindrome`)
+            return false;
+        }
+
+    } else if (validDate === false) {
+        return;
+    }
+}
+
+
+isPalindrom("24/011/1992");
+isPalindrom("31/06/2024"); // pas de 31/06
+isPalindrom("29/02/2024");
+isPalindrom("11/02/2011");
